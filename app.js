992e85b19be577adaeac451c21d060bfd6a079f2
MrColaCandy <MahmoodKhdadady10@gmail.com>
2023-08-01 21:30:47 +0430

    fixed event issues

--- app.js
+++ app.js
@@ -2,101 +2,102 @@ const display = document.querySelector(".display input");
 const keys = document.querySelector(".keys");
 
 keys.addEventListener("click", (e) => {
+  if (e.target.className !== "key") return;
   if (e.target.innerText === "=") {
     evaluate();
     return;
   }
-  if (e.target.className === "key") {
-    if (!/\d/.test(e.target.innerText)) {
-      if (
-        e.target.innerText != "e" &&
-        e.target.innerText != "π" &&
-        e.target.innerText != "-"
-      ) {
-        if (display.value === "") return;
-      }
-    }
-    if (e.target.innerText == "AC") {
-      display.value = "";
-      return;
-    }
-    if (e.target.innerText === "CE") {
-      let value = display.value.toString();
-      value = value.substring(0, value.length - 1);
-      display.value = value;
-      return;
-    }
 
-    if (e.target.innerText === "x²") {
-      evaluate();
-
-      display.value = Math.pow(display.value, 2);
-      return;
-    }
-    if (e.target.innerText === "√") {
-      evaluate();
-      display.value = Math.sqrt(display.value);
-      return;
-    }
-    if (e.target.innerText === "log") {
-      evaluate();
-      if (display.value < 0 || display.value === "-") {
-        display.value = "Can't Calculate log of negative numbers!";
-        return;
-      }
-      display.value = Math.log10(display.value);
-      return;
-    }
-    if (e.target.innerText === "π") {
-      evaluate();
-      display.value = Math.PI;
-      return;
-    }
-    if (e.target.innerText === "e") {
-      evaluate();
-      display.value = Math.E;
-      return;
-    }
-    if (e.target.innerText === "tan") {
-      evaluate();
-      display.value = Math.tan(display.value);
-      return;
-    }
-    if (e.target.innerText === "sin") {
-      evaluate();
-      display.value = Math.sin(display.value);
-      return;
-    }
-    if (e.target.innerText === "cos") {
-      evaluate();
-      display.value = Math.cos(display.value);
-      return;
-    }
-    if (e.target.innerText === "tan") {
-      evaluate();
-      display.value = Math.tan(display.value);
-      return;
-    }
-    if (e.target.innerText === "%") {
-      evaluate();
-      display.value = display.value / 100;
-      return;
-    }
-
-    if (e.target.innerText === "x!") {
-      evaluate();
-      let value = 1;
-
-      while (display.value > 0) {
-        value *= display.value;
-        display.value--;
-      }
-
-      display.value = value;
-
-      return;
+  if (!/\d/.test(e.target.innerText)) {
+    if (
+      e.target.innerText != "e" &&
+      e.target.innerText != "π" &&
+      e.target.innerText != "-"
+    ) {
+      if (display.value === "") return;
     }
   }
+  if (e.target.innerText == "AC") {
+    display.value = "";
+    return;
+  }
+  if (e.target.innerText === "CE") {
+    let value = display.value.toString();
+    value = value.substring(0, value.length - 1);
+    display.value = value;
+    return;
+  }
+
+  if (e.target.innerText === "x²") {
+    evaluate();
+
+    display.value = Math.pow(display.value, 2);
+    return;
+  }
+  if (e.target.innerText === "√") {
+    evaluate();
+    display.value = Math.sqrt(display.value);
+    return;
+  }
+  if (e.target.innerText === "log") {
+    evaluate();
+    if (display.value < 0 || display.value === "-") {
+      display.value = "Can't Calculate log of negative numbers!";
+      return;
+    }
+    display.value = Math.log10(display.value);
+    return;
+  }
+  if (e.target.innerText === "π") {
+    evaluate();
+    display.value = Math.PI;
+    return;
+  }
+  if (e.target.innerText === "e") {
+    evaluate();
+    display.value = Math.E;
+    return;
+  }
+  if (e.target.innerText === "tan") {
+    evaluate();
+    display.value = Math.tan(display.value);
+    return;
+  }
+  if (e.target.innerText === "sin") {
+    evaluate();
+    display.value = Math.sin(display.value);
+    return;
+  }
+  if (e.target.innerText === "cos") {
+    evaluate();
+    display.value = Math.cos(display.value);
+    return;
+  }
+  if (e.target.innerText === "tan") {
+    evaluate();
+    display.value = Math.tan(display.value);
+    return;
+  }
+  if (e.target.innerText === "%") {
+    evaluate();
+    display.value = display.value / 100;
+    return;
+  }
+
+  if (e.target.innerText === "x!") {
+    evaluate();
+    let value = 1;
+
+    while (display.value > 0) {
+      value *= display.value;
+      display.value--;
+    }
+
+    display.value = value;
+
+    return;
+  }
+
   if (
     display.value.length > 0 &&
     isOperator(e.target.innerText) &&
